8ef1b021b6cce3bd8e10b3a3672396d8800703b3
Dyllon Symington <dyllon.leigh@gmail.com>
2025-06-24 23:07:52 +0200

    Added a few Classes

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from turtle import Screen
+from scoreboard import Scoreboard
+from ball import Ball
+from paddle import Paddle
 
 screen = Screen()
 screen.title("Pong")
 screen.bgcolor("black")
 screen.tracer(0)
-
 
 def test():
     print("a")
